28a029a8d84c5659c3168d155241123624b39936
cristhiandevgo <ivancristhian@hotmail.com>
2020-06-13 22:41:35 -0300

    Correção do BD

--- cadastroempresa.sql
+++ cadastroempresa.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 09-Jun-2020 às 16:07
+-- Tempo de geração: 14/06/2020 às 03:39
 -- Versão do servidor: 10.4.11-MariaDB
--- versão do PHP: 7.4.6
+-- Versão do PHP: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `Empresa`
+-- Estrutura para tabela `Empresa`
 --
 
 CREATE TABLE `Empresa` (
@@ -32,7 +32,7 @@ CREATE TABLE `Empresa` (
   `nomeEmpresarial` varchar(255) NOT NULL,
   `nomeFantasia` varchar(255) NOT NULL,
   `telefone` varchar(60) NOT NULL,
-  `cnae` varchar(9) NOT NULL,
+  `cnae` varchar(255) NOT NULL,
   `cnpj` varchar(18) NOT NULL,
   `logradouro` varchar(255) NOT NULL,
   `numero` varchar(11) NOT NULL,
@@ -44,32 +44,32 @@ CREATE TABLE `Empresa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Extraindo dados da tabela `Empresa`
+-- Despejando dados para a tabela `Empresa`
 --
 
 INSERT INTO `Empresa` (`idEmpresa`, `nomeEmpresarial`, `nomeFantasia`, `telefone`, `cnae`, `cnpj`, `logradouro`, `numero`, `bairro`, `cep`, `estado`, `cidade`, `obs`) VALUES
-(35, 'MARIA APARECIDA DE SOUZA CABRAL 39559807153', 'CIDA CABELEIREIRA', '(66) 9207-5863', 'undefined', '17965395000160', 'AV GABRIEL FERREIRA', '797', 'VILA SANTO ANTONIO', '78.600-000', 'MT', 'BARRA DO GARCAS', 'Obs'),
-(45, 'GLOBO COMUNICACAO E PARTICIPACOES S/A', 'TV/REDE/CANAIS/G2C+GLOBO SOMLIVRE GLOBO.COM GLOBOPLAY', '(21) 2155-4551/ (21) 2155-4552', 'undefined', '27865757000102', 'R LOPES QUINTAS', '303', 'JARDIM BOTANICO', '22.460-901', 'RJ', 'RIO DE JANEIRO', '');
+(45, 'GLOBO COMUNICACAO E PARTICIPACOES S/A', 'TV/REDE/CANAIS/G2C+GLOBO SOMLIVRE GLOBO.COM GLOBOPLAY', '(21) 2155-4551/ (21) 2155-4552', 'undefined', '27865757000102', 'R LOPES QUINTAS', '303', 'JARDIM BOTANICO', '22.460-901', 'RJ', 'RIO DE JANEIRO', ''),
+(49, 'MARIA APARECIDA DE SOUZA CABRAL 39559807153', 'CIDA CABELEIREIRA', '(66) 9207-5863', '96.02-5-01 - Cabeleireiros, manicure e pedicure', '17965395000160', 'AV GABRIEL FERREIRA', '797', 'VILA SANTO ANTONIO', '78.600-000', 'MT', 'BARRA DO GARCAS', '');
 
 --
--- Índices para tabelas despejadas
+-- Índices de tabelas apagadas
 --
 
 --
--- Índices para tabela `Empresa`
+-- Índices de tabela `Empresa`
 --
 ALTER TABLE `Empresa`
   ADD PRIMARY KEY (`idEmpresa`);
 
 --
--- AUTO_INCREMENT de tabelas despejadas
+-- AUTO_INCREMENT de tabelas apagadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `Empresa`
 --
 ALTER TABLE `Empresa`
-  MODIFY `idEmpresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `idEmpresa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
